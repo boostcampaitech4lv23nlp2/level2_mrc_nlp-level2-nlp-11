33eec822319b814e733e8aa0aece8c58ea417c30
SangSu <ggb04110@naver.com>
2022-12-27 04:06:36 +0000

    :art: train step per eval

--- code/train.py
+++ code/train.py
@@ -28,7 +28,8 @@ def main(conf):
     # 가능한 arguments 들은 ./arguments.py 나 transformer package 안의 src/transformers/training_args.py 에서 확인 가능합니다.
     # --help flag 를 실행시켜서 확인할 수 도 있습니다.
 
-    model_args, data_args, training_args = conf.ModelArguments , conf.DataTrainingArguments , TrainingArguments(**conf.TrainingArguments) 
+    model_args, data_args, training_args = conf.ModelArguments , conf.DataTrainingArguments , TrainingArguments(**conf.TrainingArguments)
+
 
     # [참고] argument를 manual하게 수정하고 싶은 경우에 아래와 같은 방식을 사용할 수 있습니다
     # training_args.per_device_train_batch_size = 4
@@ -97,30 +98,18 @@ def run_mrc(
     datasets: DatasetDict,
     tokenizer,
     model,
-) -> NoReturn:
+) -> None:
 
     # dataset을 전처리합니다.
     # training과 evaluation에서 사용되는 전처리는 아주 조금 다른 형태를 가집니다.
     if training_args.do_train:
-        #column_names = datasets["train"].column_names
-        train_column_name = datasets["train"].column_names
-        valid_column_name = datasets["validation"].column_names
-
-        train_question_column_name = "question" if "question" in train_column_name else train_column_name[0]
-        train_context_column_name = "context" if "context" in train_column_name else train_column_name[1]
-        train_answer_column_name = "answers" if "answers" in train_column_name else train_column_name[2]
-    
+        column_names = datasets["train"].column_names
     else:
-        valid_column_name = datasets["validation"].column_names
+        column_names = datasets["validation"].column_names
 
-
-    valid_question_column_name = "question" if "question" in valid_column_name else valid_column_name[0]
-    valid_context_column_name = "context" if "context" in valid_column_name else valid_column_name[1]
-    valid_answer_column_name = "answers" if "answers" in valid_column_name else valid_column_name[2]
-
-    # question_column_name = "question" if "question" in column_names else column_names[0]
-    # context_column_name = "context" if "context" in column_names else column_names[1]
-    # answer_column_name = "answers" if "answers" in column_names else column_names[2]
+    question_column_name = "question" if "question" in column_names else column_names[0]
+    context_column_name = "context" if "context" in column_names else column_names[1]
+    answer_column_name = "answers" if "answers" in column_names else column_names[2]
 
     # Padding에 대한 옵션을 설정합니다.
     # (question|context) 혹은 (context|question)로 세팅 가능합니다.
@@ -131,21 +120,25 @@ def run_mrc(
         data_args, training_args, datasets, tokenizer
     )
 
-    # Train preprocessing / 전처리를 진행합니다.
-    def prepare_train_features(examples):
-        # truncation과 padding(length가 짧을때만)을 통해 toknization을 진행하며, stride를 이용하여 overflow를 유지합니다.
-        # 각 example들은 이전의 context와 조금씩 겹치게됩니다.
+    def prepare_tokenizer(examples) :
         tokenized_examples = tokenizer(
-            examples[train_question_column_name if pad_on_right else train_context_column_name],
-            examples[train_context_column_name if pad_on_right else train_question_column_name],
+            examples[question_column_name if pad_on_right else context_column_name],
+            examples[context_column_name if pad_on_right else question_column_name],
             truncation="only_second" if pad_on_right else "only_first",
             max_length=max_seq_length,
             stride=data_args.doc_stride,
             return_overflowing_tokens=True,
             return_offsets_mapping=True,
-            # return_token_type_ids=False, # roberta모델을 사용할 경우 False, bert를 사용할 경우 True로 표기해야합니다.
+            return_token_type_ids=True, # roberta모델을 사용할 경우 False, bert를 사용할 경우 True로 표기해야합니다.
             padding="max_length" if data_args.pad_to_max_length else False,
         )
+        return tokenized_examples
+
+    # Train preprocessing / 전처리를 진행합니다.
+    def prepare_train_features(examples):
+        # truncation과 padding(length가 짧을때만)을 통해 toknization을 진행하며, stride를 이용하여 overflow를 유지합니다.
+        # 각 example들은 이전의 context와 조금씩 겹치게됩니다.
+        tokenized_examples = prepare_tokenizer(examples)
 
         # 길이가 긴 context가 등장할 경우 truncate를 진행해야하므로, 해당 데이터셋을 찾을 수 있도록 mapping 가능한 값이 필요합니다.
         sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
@@ -166,7 +159,7 @@ def run_mrc(
 
             # 하나의 example이 여러개의 span을 가질 수 있습니다.
             sample_index = sample_mapping[i]
-            answers = examples[train_answer_column_name][sample_index]
+            answers = examples[answer_column_name][sample_index]
 
             # answer가 없을 경우 cls_index를 answer로 설정합니다(== example에서 정답이 없는 경우 존재할 수 있음).
             if len(answers["answer_start"]) == 0:
@@ -214,17 +207,7 @@ def run_mrc(
     def prepare_validation_features(examples):
         # truncation과 padding(length가 짧을때만)을 통해 toknization을 진행하며, stride를 이용하여 overflow를 유지합니다.
         # 각 example들은 이전의 context와 조금씩 겹치게됩니다.
-        tokenized_examples = tokenizer(
-            examples[valid_question_column_name if pad_on_right else valid_context_column_name],
-            examples[valid_context_column_name if pad_on_right else valid_question_column_name],
-            truncation="only_second" if pad_on_right else "only_first",
-            max_length=max_seq_length,
-            stride=data_args.doc_stride,
-            return_overflowing_tokens=True,
-            return_offsets_mapping=True,
-            # return_token_type_ids=False, # roberta모델을 사용할 경우 False, bert를 사용할 경우 True로 표기해야합니다.
-            padding="max_length" if data_args.pad_to_max_length else False,
-        )
+        tokenized_examples = prepare_tokenizer(examples)
 
         # 길이가 긴 context가 등장할 경우 truncate를 진행해야하므로, 해당 데이터셋을 찾을 수 있도록 mapping 가능한 값이 필요합니다.
         sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
@@ -259,33 +242,20 @@ def run_mrc(
             prepare_train_features,
             batched=True,
             num_proc=data_args.preprocessing_num_workers,
-            remove_columns=train_column_name,
+            remove_columns=column_names,
             load_from_cache_file=not data_args.overwrite_cache,
         )
 
-        eval_dataset = datasets["validation"]
+    eval_dataset = datasets["validation"]
 
-        # Validation Feature 생성
-        eval_dataset = eval_dataset.map(
-            prepare_validation_features,
-            batched=True,
-            num_proc=data_args.preprocessing_num_workers,
-            remove_columns=valid_column_name,
-            load_from_cache_file=not data_args.overwrite_cache,
-        )
-
-
-    # if training_args.do_eval:
-    #     eval_dataset = datasets["validation"]
-
-    #     # Validation Feature 생성
-    #     eval_dataset = eval_dataset.map(
-    #         prepare_validation_features,
-    #         batched=True,
-    #         num_proc=data_args.preprocessing_num_workers,
-    #         remove_columns=valid_answer_column_name,
-    #         load_from_cache_file=not data_args.overwrite_cache,
-    #     )
+    # Validation Feature 생성
+    eval_dataset = eval_dataset.map(
+        prepare_validation_features,
+        batched=True,
+        num_proc=data_args.preprocessing_num_workers,
+        remove_columns=column_names,
+        load_from_cache_file=not data_args.overwrite_cache,
+    )
 
     # Data collator
     # flag가 True이면 이미 max length로 padding된 상태입니다.
@@ -313,7 +283,7 @@ def run_mrc(
 
         elif training_args.do_eval:
             references = [
-                {"id": ex["id"], "answers": ex[valid_answer_column_name]}
+                {"id": ex["id"], "answers": ex[answer_column_name]}
                 for ex in datasets["validation"]
             ]
             return EvalPrediction(
@@ -329,7 +299,7 @@ def run_mrc(
     trainer = QuestionAnsweringTrainer(
         model=model,
         args=training_args,
-        train_dataset=train_dataset,# if training_args.do_train else None,
+        train_dataset=train_dataset if training_args.do_train else None,
         eval_dataset=eval_dataset, #if training_args.do_eval else None,
         eval_examples=datasets["validation"], #if training_args.do_eval else None,
         tokenizer=tokenizer,
